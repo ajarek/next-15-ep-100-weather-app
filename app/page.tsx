d03410061d8a66c5fffb352746803774dfdc75d4
Ajarek <ajarek@poczta.onet.pl>
2025-09-27 01:07:34 +0200

    meteorological data added

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SearchCity from "@/components/search-city";
 const Home = async ({  searchParams,}:{searchParams: Promise<{ name: string }>}) => {
  
    const { name } = await searchParams as { name: string }
-   console.log(name)
+   
   return (
     <div className='w-[1216px] h-[715px] flex flex-col items-center justify-start  mt-[calc(104px-58px)] gap-8 '>
       <SearchCity query="name"/>
--- lib/getLocation.tsx
+++ lib/getLocation.tsx
@@ -1,14 +1,17 @@
 
+
 export const changeCityToGeographicData = async (city: string) => {
-  const res = await fetch(`https://nominatim.openstreetmap.org/search?q=${city}&format=json&limit=1`, {cache: 'no-store'});
-  const data = await res.json();
-  console.log('API response:', data);
+  
+  const res = await fetch(
+    `https://nominatim.openstreetmap.org/search?q=${city}&format=json&limit=1`,
+    { cache: 'no-store' }
+  )
+  const data = await res.json()
+
   return {
     latitude: data[0]?.lat || 0,
     longitude: data[0]?.lon || 0,
     city: data[0]?.display_name.split(',')[0] || city,
     countryName: data[0]?.display_name.split(',').slice(-1)[0].trim() || '',
-  };
+  }
 }
-
-
